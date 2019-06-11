18d5abdfd3c3ac5bafa864fd8715cc428ac82d66
Abhijeet <force.abhijeet@gmail.com>
2019-06-10 21:24:35 -0500

    Sonar Try

---
+++ src/Class1.java
@@ -0,0 +1,8 @@
+
+public class Class1 {
+	
+	public static String method1() {
+		return "abc"; 
+	}
+
+}
---
+++ src/Class1Test.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;  
+
+public class Class1Test {
+	
+	@Test
+	public void testMethod1() {
+		 assertEquals("abc",Class1.method1());
+	}
+
+}
